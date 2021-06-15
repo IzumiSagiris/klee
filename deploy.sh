625f6626fc1e9a72bfe7606d1ac4707e1132df91
zhangbaiyang <zhangbaiyang@hotmail.com>
2021-06-15 16:55:46 +0800

    update jenkins deploy

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-docker build -t klee:1.0 .
+docker build -t klee:1.0 . &&
 
-docker rmi $(docker images | grep "none" | awk '{print $3}')
+kubectl delete deployment -n prod klee-deployment &&
 
-kubectl delete deployment -n prod klee-deployment
+kubectl create -f pod.yaml &&
 
-kubectl create -f pod.yaml
+sleep 10s
+
+docker rmi -f $(docker images | grep "none" | awk '{print $3}')
